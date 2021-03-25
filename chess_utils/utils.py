794156ae1a844717d6683e3fc21f788c9fe7b779
RAMAMURTHI <ramamurthi@Administrators-MacBook-Pro-135.local>
2021-03-26 01:52:44 +0530

    Adding function definition for ordering the moves

--- chess_utils/utils.py
+++ chess_utils/utils.py
@@ -38,14 +38,15 @@ def search(board, depth ):
  
     global count_recursion
     count_recursion = count_recursion + 1
-    
+    print(count_recursion)
     if depth == 0:
         return evaluate(board)
     
     legal_moves = list(board.legal_moves)
-    
+    moves = order_moves(legal_moves)
+
     max_eval = -1000
-    for move in legal_moves :
+    for move in moves :
         analysis_board = board.copy()
         analysis_board.push_uci(move.uci())
         evaluation = -search(analysis_board , depth - 1 )
@@ -53,21 +54,18 @@ def search(board, depth ):
     
     return max_eval
         
-
-
+def order_moves(moves , board):
+    """Function to provide order to those moves to be analyzed for the alpha beta pruning which we plan to do in the future"""
+    ordered_moves = moves
+    return ordered_moves
     
-    print(legal_moves)
-
 
 if __name__ == "__main__":
 
-    print(count_recursion)
     board = chess.Board()
     new_board = play_test_moves(board)
-    print(CountMaterial(new_board, "White"))
-    print(CountMaterial(new_board, "Black"))
-    print(evaluate(board))
     print(new_board.turn)
     print(search(new_board, depth = 4))
     print("Number of moves Analyzed = " , count_recursion)
+
     
