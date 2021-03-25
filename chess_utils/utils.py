7ea6765fbcb9c9c54dff9aa0fbb203fbccc0e9c2
RAMAMURTHI <ramamurthi@Administrators-MacBook-Pro-135.local>
2021-03-26 00:47:55 +0530

    Testing Number of calls

--- chess_utils/utils.py
+++ chess_utils/utils.py
@@ -1,43 +1,84 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import chess
-from tests.test import play_test_moves 
-#len(board.pieces(chess.ROOK, True))
+from tests.test import play_test_moves
+
+
+count_recursion = 0
 
 pieces_points = {
-                chess.KING : 0 
-                , chess.PAWN : 1 
-                , chess.BISHOP : 3
-                ,  chess.KNIGHT : 3 
-                , chess.ROOK : 5 
-                , chess.QUEEN : 9 
-                }
+    chess.KING: 0,
+    chess.PAWN: 1,
+    chess.BISHOP: 3,
+    chess.KNIGHT: 3,
+    chess.ROOK: 5,
+    chess.QUEEN: 9,
+}
 
-def CountMaterial(board , color):
-    
+
+def CountMaterial(board, color):
+
     if color == "White":
         i = True
-    else :
+    else:
         i = False
 
-    total_points = 0 
+    total_points = 0
 
     for piece, points in pieces_points.items():
-        total_points += (len(list(board.pieces(piece, i))) * points)
+        total_points += len(list(board.pieces(piece, i))) * points
     return total_points
 
 
+def evaluate(board):
 
-def evalutate(board):
-    pass
+    white_eval = CountMaterial(board, "White")
+    black_eval = CountMaterial(board, "Black")
+    eval = white_eval - black_eval
+
+    if board.turn:
+        sign = 1
+    else:
+        sign = -1
+
+    final_eval = eval * (sign)
+    return final_eval
+
+
+def search(board, depth ):
+ 
+    global count_recursion
+    count_recursion = count_recursion + 1
+    
+    if depth == 0:
+        return evaluate(board)
+    
+    legal_moves = list(board.legal_moves)
+    
+    max_eval = -1000
+    for move in legal_moves :
+        analysis_board = board.copy()
+        analysis_board.push_uci(move.uci())
+        evaluation = -search(analysis_board , depth - 1 )
+        max_eval = max(evaluation , max_eval)
+    
+    return max_eval
+        
+
+
+    
+    print(legal_moves)
 
 
 if __name__ == "__main__":
-    
+
+    print(count_recursion)
     board = chess.Board()
     new_board = play_test_moves(board)
-    print(CountMaterial(new_board , "White"))
-    print(CountMaterial(new_board , "Black"))
-
-
-
+    print(CountMaterial(new_board, "White"))
+    print(CountMaterial(new_board, "Black"))
+    print(evaluate(board))
+    print(new_board.turn)
+    print(search(new_board, depth = 4))
+    print("Number of moves Analyzed = " , count_recursion)
+    
